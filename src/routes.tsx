cc3a54b4ccff3c364688fb1301d541b46f56e162
Renan M. Batista <renanmourabatista@gmail.com>
2020-10-16 16:36:57 -0400

    [WIP] - Criando as telas do mobile

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,13 +7,49 @@ const { Navigator, Screen } = createStackNavigator();
 
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import Header from './components/Header';
 
 export default function Routes() {
     return (
         <NavigationContainer>
-            <Navigator screenOptions={{ headerShown: false }}>
-                <Screen name="OrphanagesMap" component={OrphanagesMap}></Screen>
-                <Screen name="OrphanageDetails" component={OrphanageDetails}></Screen>
+            <Navigator screenOptions={
+                    { 
+                        headerShown: false, 
+                        cardStyle : { 
+                            backgroundColor: '#f2f3f5'
+                        } 
+                    }
+                }>
+                <Screen 
+                    name="OrphanagesMap" 
+                    component={OrphanagesMap}
+                />
+                <Screen 
+                    options={{
+                        headerShown: true,
+                        header: () => <Header showCancel={false} title="Orfanato"/>
+                    }} 
+                    name="OrphanageDetails" 
+                    component={OrphanageDetails} 
+                />
+                <Screen 
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Selecione no mapa"/>
+                    }} 
+                    name="SelectMapPosition" 
+                    component={SelectMapPosition} 
+                />
+                <Screen 
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Informe os dados"/>
+                    }} 
+                    name="OrphanageData" 
+                    component={OrphanageData}
+                />
             </Navigator>
         </NavigationContainer>
     )
